065e00546132b6279aa1e1a3d1b074b757baa334
Sutelc <157877876+Sutelc@users.noreply.github.com>
2024-12-12 12:51:31 +0100

    h1 removed

--- src/aboutpage.jsx
+++ src/aboutpage.jsx
@@ -4,7 +4,6 @@ export default function About(){
     return(
         <>
         <h1>About page</h1>
-        <h1>About page</h1>
         <a href="/">Home</a><br />
         <Link to={"/"}>Home React</Link>
         <img src="/images 2.png" alt="" />
